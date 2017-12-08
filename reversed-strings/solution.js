02ea362d719949452c00e08337972208c087420a
Richard Montgomery <rikermonty@gmail.com>
2017-12-07 16:45:26 -0800

    a different way to reversed strings kata

--- reversed-strings/solution.js
+++ reversed-strings/solution.js
@@ -3,3 +3,17 @@
 function solution(str){
   return str.split('').reverse().join('');
 }
+
+// or
+
+function solution(str){
+  let arr = [];
+  let string = str.split('');
+  let num = string.length;
+
+  while(num >= 0){
+    arr.push(string[num])
+    num--
+  }
+  return arr.join('');
+}
